732ffe3464df47da97856e0d102e86dca580ff76
James Randolph <james.randolph2016@gmail.com>
2024-10-02 06:23:28 -0400

    add keyboard visuals

--- src/Interface.js
+++ src/Interface.js
@@ -1,8 +1,35 @@
+import { useKeyboardControls } from "@react-three/drei"
+
 export default function Interface() {
+
+    const forward = useKeyboardControls((state) => state.forward);
+    const backward = useKeyboardControls((state) => state.backward);
+    const leftward = useKeyboardControls((state) => state.leftward);
+    const rightward = useKeyboardControls((state) => state.rightward);
+    const jump = useKeyboardControls((state) => state.jump);
+
+
     return (
         <div className="interface">
             <div className="time">
                 <span>00:00</span>
+            </div>
+            <div className="restart">
+                <span>Restart</span>
+            </div>
+            {/* Controls */}
+            <div className="controls">
+                <div className="raw">
+                    <div className={ `key ${ forward ? 'active' : '' }` }></div>
+                </div>
+                <div className="raw">
+                    <div className={ `key ${ leftward ? 'active' : '' }` }></div>
+                    <div className={ `key ${ backward ? 'active' : '' }` }></div>
+                    <div className={ `key ${ rightward ? 'active' : '' }` }></div>
+                </div>
+                <div className="raw">
+                    <div className={ `key large ${ jump ? 'active' : '' }` }></div>
+                </div>
             </div>
         </div>
     )
